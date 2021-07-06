2d5b034dcdd79c0928f6a9e7a95f7d0d8401851f
Aizada <Aizakoni29@gmail.com>
2021-07-06 20:45:59 +0600

    Bookstore

---
+++ bookstore/src/App.js
@@ -0,0 +1,15 @@
+import MyNavbar from './components/MyNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import Jumbotron from './components/Jumbotron';
+
+function App() {
+  return (
+    <div >
+      <MyNavbar/>
+      <Jumbotron/>
+    </div>
+  );
+}
+
+export default App;
